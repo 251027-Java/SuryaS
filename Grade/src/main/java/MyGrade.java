c36c184753612f0a745f768318531fff654478e4
snsorl <suryasolaiappan@gmail.com>
2025-10-29 13:44:24 -0500

    mygrade recently..

--- Grade/src/main/java/MyGrade.java
+++ Grade/src/main/java/MyGrade.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 public class MyGrade {
 
     public static void main(String[] args) {
+        System.out.println("Enter a grade.");
         Scanner userInput = new Scanner(System.in);
         double input;
-        try {
-            input = userInput.nextDouble();
-            if (input > 100 || input < 0) System.out.println("Your grade is out of bounds.");
-            else {
-                char grade = '0';
-                if (input >= 90) grade = 'A';
-                else if (input >= 80) grade = 'B';
-                else if (input >= 70) grade = 'C';
-                else if (input >= 60) grade = 'D';
-                else grade = 'F';
-                System.out.println("Your grade is " + grade + ".");
+        boolean validInput = true;
+        while (validInput) {
+            try {
+                input = userInput.nextDouble();
+                if (input > 100 || input < 0) validInput = false;
+                char letterGrade = 'O';
+                if (input >= 90) letterGrade = 'A';
+                else if (input >= 80) letterGrade = 'B';
+                else if (input >= 70) letterGrade = 'C';
+                else if (input >= 60) letterGrade = 'D';
+                else letterGrade = 'F';
+                System.out.println("Your grade is " + letterGrade + ".");
+                validInput = true;
+            } catch (Exception e) {
+                System.out.println("You were supposed to enter a number between 0 and 100. Enter a grade.");
+                userInput.next();
             }
-        } catch (Exception e) {
-            System.out.println("You were supposed to enter a grade.");
         }
-    }
 
+    }
 }
