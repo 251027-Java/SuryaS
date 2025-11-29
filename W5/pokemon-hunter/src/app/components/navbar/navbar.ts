8f02085625dfb9a1758e4ad22b1d3411426e4ec9
snsorl <suryasolaiappan@gmail.com>
2025-11-29 03:32:57 -0600

    logout has expiry date but it will expire soon

--- W5/pokemon-hunter/src/app/components/navbar/navbar.ts
+++ W5/pokemon-hunter/src/app/components/navbar/navbar.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,  
   imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(public auth: Auth) {}
 
+  logout() {
+    this.auth.logout();
+  }
 }
